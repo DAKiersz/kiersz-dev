03d9b3e77ca1754af368d9cf60b22290d1481683
DAKiersz <59928295+DAKiersz@users.noreply.github.com>
2023-05-13 12:16:53 +0100

    Update infra; fix cicd

--- infra/kierszdev.tf
+++ infra/kierszdev.tf
@@ -37,3 +37,12 @@ resource "azurerm_static_site" "staticsite" {
   sku_size            = "Free"
   sku_tier            = "Free"
 }
+
+# Azure Application Insights
+resource "azurerm_application_insights" "appinsights" {
+  name                = "appinsights-${local.name}-${local.environment}-${local.location}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+  tags                = local.common_tags
+}
